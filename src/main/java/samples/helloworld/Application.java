90063a403e53751a73ab5236d947e02e9a017d56
Jan Nielsen <jan.sture.nielsen@gmail.com>
2021-04-04 10:34:08 -0600

    Polish -- tabs to spaces.

--- src/main/java/samples/helloworld/Application.java
+++ src/main/java/samples/helloworld/Application.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-	String who = "world";
+        String who = "world";
 
-	if (0 < args.length) {
-	    who = String.join(" ", Arrays.asList(args));
-	}
+        if (0 < args.length) {
+            who = String.join(" ", Arrays.asList(args));
+        }
 
-	System.out.println(String.format("Hello %s!", who));
+        System.out.println(String.format("Hello %s!", who));
     }
 
 }
